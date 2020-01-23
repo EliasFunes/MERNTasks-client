ad7fd54f17524d06db5c9de995a589ac60db88bc
Elias Funes <sk8flip720@gmail.com>
2020-01-22 23:50:24 -0300

    First commit

---
+++ src/components/auth/Login.js
@@ -0,0 +1,78 @@
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
+
+const Login = () => {
+
+    const [usuario, guardarUsuario] = useState({
+        email: '',
+        password:''
+    });
+
+    const {email, password} = usuario;
+
+    const handleChange = e => {
+        guardarUsuario({
+            ...usuario,
+            [e.target.name]:e.target.value
+        });
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //validar campos vacios
+        if(email.trim() === '' || password.trim() === ''){
+            return;
+        }
+    }
+
+    return (
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesión</h1>
+                <form
+                    onSubmit={handleSubmit}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu Email"
+                            value={email}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu Password"
+                            value={password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    
+                    <div className="campo-form">
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Iniciar Sesión"
+                        />
+                    </div>
+                </form>
+
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Obtener Cuenta
+                </Link>
+
+            </div>
+        </div>
+    );
+};
+
+export default Login;
