e326bcc9a0cc6a517662677f00062ee0603e16d5
Elias Funes <eliasfunesriveros@gmail.com>
2020-02-05 08:13:36 -0300

    Adding alerts context and new user validations

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,7 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {Link} from "react-router-dom";
+import alertaContext from "../../context/alertas/alertaContext";
 
 const NuevaCuenta = () => {
+
+    //extraer valores del context
+    const alertasContext = useContext(alertaContext);
+    const {alerta, mostrarAlertaFn} = alertasContext;
 
     const [usuario, guardarUsuario] = useState({
         nombre: '',
@@ -23,19 +28,29 @@ const NuevaCuenta = () => {
         e.preventDefault();
 
         //validar campos vacios
-        if(email.trim() === '' || password.trim() === ''){
+        if(nombre.trim() === '' || email.trim() === '' || password.trim() === '' || confirmar.trim() === ''){
+            mostrarAlertaFn('Todos los campos son obligatorios', 'alerta-error');
             return;
         }
 
         //Password minimo de 6 caracteres
+        if(password.length < 6){
+            mostrarAlertaFn('El password debe ser de al menos 6 caracteres', 'alerta-error');
+            return;
+        }
 
         //los 2 passwords son iguales
+        if(password !== confirmar) {
+            mostrarAlertaFn('Los passwords no son iguales', 'alerta-error');
+            return;
+        }
 
         //pasarlo al action
     }
 
     return (
         <div className="form-usuario">
+            {alerta ? (<div className={`alerta ${alerta.categoria}`}>{alerta.msg}</div>) : null}
             <div className="contenedor-form sombra-dark">
                 <h1>Crear Cuenta</h1>
                 <form
